60217c3c656af749231f9dae034308454c1820eb
maheshgoud543 <goudsmahesh123@gmail.com>
2024-08-06 21:12:03 +0530

    My portfolio

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC, where I dive into the world of programming. I primarily work with JavaScript and React these days, having built several websites and applications in these technologies. Beyond web development, I have a strong interest in CyberSecurity. Regardless of the language or technology, my passion lies in programming and solving complex problems. Feel free to check out my projects and contributions on my <a href="https://github.com/maheshgoud543" target="_blank">GitHub profile</a>!`,
+
+    "cs-degree": `This is my B.Tech degree, proudly displayed on my wall. While the coursework was quite theoretical, it provided me with a solid foundation in computer science principles. This background has been invaluable in understanding and applying complex concepts in real-world projects.`,
+
+    "sofa-table": `That's my sofa, a perfect spot for relaxation and binge-watching my favorite Movies/Series. It's also where I often find inspiration for new ideas and projects.`,
+
+    tv: `Here's my TV, where I've been diving into tech content lately. Some of my favorite YouTubers include <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">ThePrimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>, <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a>, and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>. Their content has been a great source of learning and inspiration.`,
+
+    bed: `This is where I sleep and often where I come up with my best ideas. The comfort of my bed seems to spark creativity, and I make sure to jot down any ideas before they slip away.`,
+
+    resume: `On this desk, you'll find my resume. <a href="https://drive.google.com/file/d/18oMjz6ZxT-1m-QjcfViHEuePMcs0B_AB/view?usp=drive_link" target="_blank">Take a look!</a> I'm always open to new opportunities and challenges. Feel free to reach out to me at goudsmahesh123@gmail.com for any exciting job opportunities!`,
+
+    projects: `I've built several projects using React.js, showcasing my skills in front-end development. Here are a few of them: <a href="https://onepieceisreal.netlify.app/">One Piece</a> (a fan site), <a href="https://shopsper.netlify.app/">Shopping Website</a> (an e-commerce platform), and <a href="https://temptoday.netlify.app/">Temp Website</a> (a weather app). I hope you find them interesting!`,
+
+    library: `With 1.5 years of experience in React.js, I've developed a strong proficiency in building dynamic and responsive web applications. My skill set also includes JavaScript, HTML, and CSS, making me well-equipped to tackle a variety of front-end development tasks.`,
+
+    exit: `If you want to exit Mahesh's portfolio, just close the tab. Thank you for visiting!`,
+};
